2210287debf2602d5f411112bfa1b7b3d58a140a
npc-z <1763998996@qq.com>
2024-08-04 22:35:41 +0800

    better search result

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,11 @@ pub struct Config {
     search_path: String,
 }
 
+pub struct SearchResult {
+    line: u16,
+    content: String,
+}
+
 impl Config {
     pub fn build(args: &Vec<String>) -> Result<Config, &'static str> {
         if args.len() < 3 {
@@ -19,13 +24,33 @@ impl Config {
     }
 }
 
-pub fn run(config: Config) {
+fn search(config: Config) -> Vec<SearchResult> {
     let contents = fs::read_to_string(config.search_path).expect("read file failed");
-    let result = contents
-        .lines()
-        .filter(|line| line.to_lowercase().contains(&config.query));
-
-    for r in result {
-        println!("found: {r}");
+    let mut result: Vec<SearchResult> = vec![];
+    let mut index: u16 = 1;
+    for line in contents.to_lowercase().lines() {
+        if line.contains(&config.query) {
+            result.push(SearchResult {
+                line: index,
+                content: line.to_string(),
+            })
+        }
+        index += 1;
     }
+
+    return result;
+}
+
+fn display_search_result(result: Vec<SearchResult>) {
+    for r in result {
+        let line = r.line;
+        let c = r.content;
+        println!("line: {line}");
+        println!("\t{c}");
+    }
+}
+
+pub fn run(config: Config) {
+    let result = search(config);
+    display_search_result(result);
 }
